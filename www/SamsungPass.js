654df94785c57fb972b0ac1d4d9e74554fb0ad74
Jurijs Jeskins <jurijs.jeshkins@gmail.com>
2016-07-15 15:13:46 +0300

    Rewrite of deprecated parts
    Rewrote SamsungPass to define/require scheme

--- www/SamsungPass.js
+++ www/SamsungPass.js
@@ -1,30 +1,26 @@
-function SamsungPass() {
+cordova.define("window.plugins.samsungpass", function(require, exports, module) {
+    var exec = require("cordova/exec");
+
+    function SamsungPass() {
+    }
+
+    SamsungPass.prototype.isAvailable = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "SamsungPass", "isSamsungPassSupported", []);
+    };
+
+    SamsungPass.prototype.hasRegisteredFingers = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "SamsungPass", "hasRegisteredFingers", []);
+    };
+
+    SamsungPass.prototype.getFingerprintName = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "SamsungPass", "getFingerprintName", []);
+    };
+
+    SamsungPass.prototype.startIdentify = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "SamsungPass", "startIdentify", []);
+    };
+
+
+    var samsungPass = new SamsungPass();
+    module.exports = samsungPass;
 }
-
-SamsungPass.prototype.isAvailable = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SamsungPass", "isSamsungPassSupported", []);
-};
-
-SamsungPass.prototype.hasRegisteredFingers = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SamsungPass", "hasRegisteredFingers", []);
-};
-
-SamsungPass.prototype.getFingerprintName = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SamsungPass", "getFingerprintName", []);
-};
-
-SamsungPass.prototype.startIdentify = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "SamsungPass", "startIdentify", []);
-};
-
-
-SamsungPass.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-
-  window.plugins.SamsungPass = new SamsungPass();
-  return window.plugins.SamsungPass;
-};
-
-cordova.addConstructor(SamsungPass.install);
